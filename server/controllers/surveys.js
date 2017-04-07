cbb6bde850a2eab6c2c4bc38e2c47beefcc983a8
RyanYizhaoHuang <ryan.yizhao@gmail.com>
2017-04-07 14:24:37 -0400

    update timezone

--- server/controllers/surveys.js
+++ server/controllers/surveys.js
@@ -59,7 +59,8 @@ module.exports.CreateSurvey = (req, res) => {
     try {
 
 
-        let currentDate = moment.utc(moment()).format(); 
+        let currentDate = moment().format(); 
+        console.log(currentDate);
         let expireDate =  moment.utc(Date.parse(req.body.expireDate)).format();
         console.log(expireDate);
         //create question objects
